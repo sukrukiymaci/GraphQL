7287fcd0dcf822a261c3432d5dbd35c16f8cea61
sukrukiymaci <sukrukiymaci@gmail.com>
2021-10-14 20:14:15 -0700

    Basic graphQL functionality

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server');
+const SessionAPI = require('./datasources/sessions')
+
+const typeDefs = require("./schema.js");
+
+const resolvers = require('./resolvers.js');
+
+const dataSources = () => ({
+    sessionAPI: new SessionAPI()
+});
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+
+server
+    .listen({ port: process.env.PORT || 4000 })
+    .then(({ url }) => {
+        console.log(`GraphQL running on port ${url}`);
+    })
